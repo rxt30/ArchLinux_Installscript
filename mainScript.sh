e5b6b640a3f0818da9afa63d5b4c4f66fb83d58a
rxt30 <mhampel@pm.me>
2021-01-05 12:26:04 +0100

    Added instruction for encrypted devices, customized install process

--- mainScript.sh
+++ mainScript.sh
@@ -5,14 +5,34 @@ endif
 
 echo "Installing base System"
 cp mirrorlist /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devle linux linux-firmware vim man-db man-pages texinfo wpa_supplicant wireless_tools netctl dialog
-if[ $2 = "intel" ]
+pacstrap /mnt base base-devel linux linux-firmware
+pacstrap /mnt vim man-db man-pages texinfo 
+
+echo "Install tools for wireless connection? [y/n]"
+read -q installDecision
+if [ $installDecision = "y" ] 
+then
+    pacstrap /mnt wpa_supplicant wireless_tools netctl dialog
+fi
+
+echo "Install tools for encrypted partitions? [y/n]"
+read -q installDecision
+if [ $installDecision = "y" ]
+then
+    pacstrap /mnt dosfstools gptfdisk lvm2
+fi
+
+echo "Install intel ucode? [y/n]"
+read -q installDecision
+if [ $installDecision = "y"]
+then
     pacstrap /mnt intel-ucode
-endif
+fi
 
 echo "Generating fstab"
 genfstab -Lp /mnt > /mnt/etc/fstab
 
 echo "Starting the System"
 cp systemConfigure.sh /mnt/
-arch-chroot /mnt /mnt/systemConfigur.sh
+#arch-chroot /mnt /mnt/systemConfigur.sh
+arch-chroot /mnt
