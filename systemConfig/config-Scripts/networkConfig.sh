bcbbd269aca5b9d11a303a6ed83285f22ea80800
rxt30 <mhampel@pm.me>
2021-01-07 14:09:47 +0100

    Small fixes

--- systemConfig/config-Scripts/networkConfig.sh
+++ systemConfig/config-Scripts/networkConfig.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+pacman -S dhcp
 echo "Please select a interface to use for the connection (wireless interfaces usually start with w)"
 select networkInterface in $(ip -brief link | awk '{print $1}')
 do
@@ -10,5 +11,6 @@ do
         systemctl enable netctl-auto@$networkInterface
     fi
     dhcpcd $networkInterface
+    break;
 done
 systemctl enable dhcpcd
