d77527430d6ba3ef62600a8395f9e1724fc57d89
rxt30 <mhampel@pm.me>
2021-01-09 12:17:18 +0100

    Added select for texteditor

--- mainScript.sh
+++ mainScript.sh
@@ -8,7 +8,18 @@ fi
 echo "Installing base System"
 cp mirrorlist /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware zsh
-pacstrap /mnt vim man-db man-pages texinfo 
+pacstrap /mnt man-db man-pages texinfo 
+
+echo "Choose your cli editor to install:"
+select editor in vim nano
+do
+    case $editor in
+        vim|nano)
+            pacstrap /mnt $editor
+            break
+            ;;
+    esac
+done
 
 echo "Install tools for wireless connection? [y/n]"
 read installDecision
